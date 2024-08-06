4b74c026e97b3b3c83a5108ef462037698c34c39
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-08-06 12:21:03 +0300

    created a project in xcode

---
+++ OnlineShop/OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Khusrav Safiev on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
